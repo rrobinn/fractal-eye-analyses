f55f29b1c8d7f0911704336a0ce59ac97f1dd4d7
Robin <sifre002$umn.edu>
2020-01-29 11:36:49 -0600

    update MFDFA_settings to output a struct()

--- MFDFA/MFDFA_settings.m
+++ MFDFA/MFDFA_settings.m
@@ -1,14 +1,10 @@
-function [m, scres, q] =  MFDFA_settings()
+function [settings] =  MFDFA_settings()
+%% function to create struct() that contains user-defined MFDFA settings 
 % Edit this function to change the settings 
-
-%Polynomial order for detrending, to be looped through
-% M=[2:3]; %m=1(linear); 2(quadratic); 3(cubic)
-m=2;
-%Total number of segment sizes, to be looped through
-% Scres=[4 9];
-scres=4;
-
-%q-order exponents for MFDFA calculation
-q=[-5,-3,-1,0,1,3,5]; 
+settings = struct();
+settings.m = 2; %Polynomial order for detrending. m=2 is quadratic 
+settings.scres = 4; %Total number of segment sizes, to be looped through
+settings.q = [-5,-3,-1,0,1,3,5]; %q-order exponents for MFDFA calculation
+settings.scmin =4; %from prev lit
 
 end

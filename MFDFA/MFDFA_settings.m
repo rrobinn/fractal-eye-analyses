c03726cd745e6df42d296b6cc68368021b320a5f
Robin <sifre002$umn.edu>
2020-02-20 08:34:15 -0600

    add setting for plotting r2 figs

--- MFDFA/MFDFA_settings.m
+++ MFDFA/MFDFA_settings.m
@@ -6,5 +6,7 @@ settings.m = 2; %Polynomial order for detrending. m=2 is quadratic
 settings.scres = 4; %Total number of segment sizes, to be looped through
 settings.q = [-5,-3,-1,0,1,3,5]; %q-order exponents for MFDFA calculation
 settings.scmin =4; %from prev lit
+settings.minTimeSeriesLength = 1000;
+settings.r2plot = 1; % flag for plotting & saving r^2 figures
 
 end

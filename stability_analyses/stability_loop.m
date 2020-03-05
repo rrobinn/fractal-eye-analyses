66ad2c31878c8cf489f9506e1f8f492d59b57450
U-CEHD-W1651348\Robin <Robin@CEHD-W1651348.ad.umn.edu>
2020-03-05 15:33:27 -0600

    only make figures for every tenth session

--- stability_analyses/stability_loop.m
+++ stability_analyses/stability_loop.m
@@ -1,35 +1,36 @@
-For each participant:
-    For each time series:
-        Create truncated time series 
+% For each participant:
+%     For each time series:
+%         Create truncated time series 
 
 clear all
 close all
 % set wdir
-wdir = '/Users/sifre002/Box/sifre002/18-Organized-Code/fractal-eye-analyses/';
+wdir = 'C:\Users\Robin\Documents\fractal-eye-analyses';
 cd([wdir '/MFDFA/'])
-datadir = '/Users/sifre002/Box/Dancing Ladies share/IndividualData/All_2018_12_11_DL/';
-figdir = '/Users/sifre002/Box/Dancing Ladies share/R2_figures/scres19_scmin_6/';
+%datadir = '/Users/sifre002/Box/Dancing Ladies share/IndividualData/All_2018_12_11_DL/';
+datadir = 'C:\Users\Robin\Box\Dancing Ladies share\IndividualData\All_2018_12_11_DL\';
+% figdir = '/Users/sifre002/Box/Dancing Ladies share/R2_figures/scres19_scmin_6/';
+figdir = 'C:\Users\Robin\Box\sifre002\11_Figures\mfdfa-stability\';
+
 % read participant list
-p = importdata('/Users/sifre002/Box/sifre002/9_ExcelSpreadsheets/Dancing_Ladies/ParticipantLists_DL/20200219_ParticList.csv');
-%
-%group_out = {};
+%p = importdata('/Users/sifre002/Box/sifre002/9_ExcelSpreadsheets/Dancing_Ladies/ParticipantLists_DL/20200219_ParticList.csv');
+p = importdata('C:\Users\Robin\Box\sifre002\9_ExcelSpreadsheets\Dancing_Ladies\ParticipantLists_DL\20200219_ParticList.csv');
+
 errors = {};
 
 %% temporarily disable polynomial warnings
 
-w = warning('query','last');
-id = w.identifier;
-warning('off',id);
-
-
+%w = warning('query','last');
+%id = w.identifier;
+%warning('off',id);
 %%
 for i = 1:length(p)
     tic();
     display(['Calculating H for ' num2str(i) ' out of ' num2str(length(p))]);
     id = p{i};
     
-    % only make plots for every 7th session to save time 
-    if mod(i,7)==0
+    % only make plots for every 10th session to save time 
+    if mod(i,10)==0
         [settings] =  MFDFA_settings('r2plot', 1, 'scres', 19, 'scmin', 16);
     else
         [settings] =  MFDFA_settings('r2plot', 0, 'scres', 19, 'scmin', 16); 
@@ -45,6 +46,8 @@ for i = 1:length(p)
         % combinate data and specs
         specs = [specs_out_calver; specs_out_et];
         ts_out = [ts_out_calver; ts_out_et];
+        clear specs_out_calver specs_out_et ts_out_calver ts_out_et;
+        specs(:,1) = {id}; % update id to include session number
         %% do dfa
         h = cell(size(specs,1),1);
         r = cell(size(specs,1),1);
@@ -52,6 +55,7 @@ for i = 1:length(p)
         for t=1:size(ts_out,1)
             ts = ts_out{t};
             [H, r2] = calculate_H_monofractal(ts, 'settings', settings);
+
             mov = specs{t,2}; mov = mov(1:regexp(mov, '\.')-1);
             seg = specs{t,3};
             if isa(seg, 'double')

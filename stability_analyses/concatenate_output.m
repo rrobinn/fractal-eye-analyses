6171e8efe69fc5f2a82444c80d5d555d9958cf5f
Robin <sifre002$umn.edu>
2020-03-19 09:56:32 -0500

    write output to .txt

--- stability_analyses/concatenate_output.m
+++ stability_analyses/concatenate_output.m
@@ -1,26 +1,52 @@
-% concatenate data
+% Script to read in data from each individual folder in a given path
+% (specified by "folder") and concatenate data into a single data frame
+
 folder = '/Users/sifre002/Documents/Code/fractal-eye-analyses/out/stability_analyses/parameter-stability-full-ts/';
 files = dir(folder);
 dirFlags = [files.isdir];
 files = files(dirFlags);
-
+errors= {};
 for f = 1:size(files,1)
    
     try
+        % Read in .mat file w/ stability analysis output for this
+        % participant
+        disp(['Attempting to read in data for ' files(f).name]);
         load([folder files(f).name '/stability_analysis.mat']);
-        h_out = h_out(~cellfun(@isempty, h_out(:,1)), :); % remove empty cells - these are segments that errored out in stability_loop.m
-        %n_rows = size(params,1) * size(h_out,1);
+ 
+        
+        % remove empty cells - these are segments that errored out in stability_loop.m
+        h_out = h_out(~cellfun(@isempty, h_out(:,1)), :); 
+        
+        % Pre-allocate output
         n_params = size(params,1);
-        
-        
+        n_rows = n_params * size(h_out,1);
+        particData = zeros(n_rows, 5);
+
         % duplicate trial info (e.g. id, movie, segment, etc.) n times, where n=# of parameters
         specs = cellfun(@(x) repmat(x, n_params ,1), h_out(:,1), 'UniformOutput', false);
+        specs = cat(1, specs{:,1});
         
-        h_r = cat(1, h_out{:,2}); % combine the [h,r] output for each segment
-        rep_params = repmat(params, size(h_out,1), 1); % create matrix of copied params (copied n times, where n=# of segments)
+        % duplicate parameter info (copied n times, where n=# of segments)
+        rep_params = repmat(params, size(h_out,1), 1); 
 
+        % combine the [h,r] output for each segment
+        h_r = cat(1, h_out{:,2});
         
+        % write trial info into separate file
+        fid = fopen('specs.txt', 'w');
+        for s = 1:size(specs,1)
+           temp = [specs{s,1} ',' specs{s,2} ',' specs{s,3}, ',' num2str(specs{s,4}), ...
+               ',',num2str(specs{s,5}),',', num2str(specs{s,6}), ',' , num2str(specs{s,7}), ...
+               num2str(rep_params(s,1)),',', num2str(rep_params(s,2)),',',num2str(rep_params(s,3)), ',', ...
+               num2str(h_r(s,1)), ',' num2str(h_r(s,2))];
+           fprintf(fid, '%s \n', temp);
+        end
+        
+     
+
     catch ME
+        
         
     end
     

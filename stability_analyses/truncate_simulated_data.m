4985244b9f6f73f695d1757b617978c806bf9755
Robin <sifre002$umn.edu>
2020-11-10 08:38:35 -0600

    remove padding from time series before truncating

--- stability_analyses/truncate_simulated_data.m
+++ stability_analyses/truncate_simulated_data.m
@@ -40,18 +40,30 @@ simData=load([datadir 'simTimeSeries.csv']);
 for s=1:size(simData,1)
     display(['Truncating simulated series #' num2str(s)]);
     
-    % Pull simulated time series 
+    % Pull simulated time series
     ts=simData(s,:);
     
+    % Make sure missing data are only at the end of the series
+    % (999 is padding at the end of ts to ensure that each time series has the same dimension)
+    padding = find(ts==999);
+    if length(padding)==1 && padding==length(ts) % Case where there is only one missing value at the end
+        ts=ts(ts~=999); % Trim the end of the time series if coded as 999
+    else
+        assert(unique(diff(find(ts==999))) == 1, ...
+            'Unexpected missing data pattern. Missing data are not all contiguous');
+        ts=ts(ts~=999);
+    end
+    
+
     % preallocate output for holding indices for truncating
-    out = cell(1, 3); 
+    out = cell(1, 3);
     
     % Generate id for time series
     id=['Row' num2str(s)];
     
     % get indices for truncating from beginning, truncating from end
-    [begInd, endInd] = TruncateIndices(ts, constantSegLength, 'settings', settings); 
-    truncs=[begInd;endInd]; 
+    [begInd, endInd] = TruncateIndices(ts, constantSegLength, 'settings', settings);
+    truncs=[begInd;endInd];
     
     % Create truncated versions
     temp=cell(length(truncs),1);
@@ -67,9 +79,9 @@ for s=1:size(simData,1)
     out{1,2} = truncs;
     out{1,3} = temp;
     
-    % Directory for this time series 
+    % Directory for this time series
     thisdir = [outdir id '/'];
-    if ~exist(thisdir) 
+    if ~exist(thisdir)
         mkdir(thisdir)
     end
     save([thisdir id '_truncated_ts.mat'], 'out');

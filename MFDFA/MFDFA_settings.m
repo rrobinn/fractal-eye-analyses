0401deb1be007a01f7fe62046934fa24a79979b9
Robin <sifre002$umn.edu>
2020-11-10 06:29:10 -0600

    update default parameters

--- MFDFA/MFDFA_settings.m
+++ MFDFA/MFDFA_settings.m
@@ -3,8 +3,8 @@ function [settings] =  MFDFA_settings(varargin)
 % Edit this function to change the settings 
 settings = struct();
 settings.m = 2; %Polynomial order for detrending. m=2 is quadratic 
-settings.scres = 4; % Total number of segment sizes, to be looped through
-settings.scmin =4; %Minimum scale size from prev lit
+settings.scres = 8; % Total number of segment sizes, to be looped through
+settings.scmin =8; %Minimum scale size from prev lit
 settings.minTimeSeriesLength = 1000;
 settings.scmaxDiv = 4;
 settings.q = [-5,-3,-1,0,1,3,5]; %q-order exponents for MFDFA calculation

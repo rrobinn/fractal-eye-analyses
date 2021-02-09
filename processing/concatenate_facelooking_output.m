20dcb4fc9be8f33046706e9d15576488f3c4f690
Robin <sifre002@umn.edu>
2021-02-09 11:41:05 -0600

    Create concatenate_facelooking_output.m

---
+++ processing/concatenate_facelooking_output.m
@@ -0,0 +1,63 @@
+clear all
+close all
+% set wdir
+wdir = '/Users/sifre002/Documents/Code/fractal-eye-analyses';
+cd([wdir '/MFDFA/'])
+
+%%
+folder = '/Users/sifre002/Box/sifre002/7_MatFiles/01_Complexity/Individual_Data/20200803data/'; % Folder w/ individual data, where concatenated output will be written
+outputFileName = 'face_out.txt'; % Name for output file with concatenated data
+header = 'id, movie, seg, longestFixDur, faceCount, otherCount'; 
+%%
+files = dir(folder);
+dirFlags = [files.isdir];
+files = files(dirFlags);
+readErrors= {};
+
+%%
+fid = fopen([folder outputFileName], 'w');
+fprintf(fid, '%s \n', header);
+for f = 1:size(files,1)
+   try
+       % Read in .mat file w/ segmented data
+        session = files(f).name;
+        disp(['Attempting to read in data for ' session]);
+        load([folder session '/' session '_segmentedTimeSeries.mat']);
+       
+        
+         % Get rid of empty cells 
+         segmentedData = segmentedData(~cell2mat( cellfun(@(x) isempty(x), segmentedData(:,1), 'UniformOutput', false) ) );
+         for s = 1:size(segmentedData,1)
+             temp = segmentedData{s,1};
+             trial = temp{1, segSummaryCol.trial};
+             seg = temp{1, segSummaryCol.seg}; 
+             %%  Calculate longestFixDur, faceCount, otherCount
+             % Longest fixation
+             longestFixBool = cell2mat(temp(:, segSummaryCol.longestFixBool));
+             longestFix = sum(longestFixBool);
+             
+             % Count frames on faces vs. somewhere else (1,2,3=faces,
+             % 0=elsewhere, -9999=missing)
+             aoi = cell2mat(temp(longestFixBool, segSummaryCol.aoi)); % Pull AOI data during the longest fixation
+             faceCount = sum(aoi==1 | aoi==2 | aoi==3);
+             otherCount = sum(aoi ==0);
+             
+             %% write data to txt file 
+             header = 'id, movie, seg, longestFixDur, faceCount, otherCount'; 
+
+             temp_out = [session ',' trial ',' num2str(seg) ',' ...
+                 num2str(longestFix) ',' num2str(faceCount) ',' num2str(otherCount)];
+             fprintf(fid, '%s \n', temp_out);
+         end
+         
+     
+        
+   catch ME
+        readErrors = horzcat(readErrors, ME.message);
+        disp(['ERROR Did not find file for ' files(f).name]);
+   end
+end
+readErrors=readErrors';
+%%
+save([folder 'face_out_readErrors.mat'], 'readErrors');
+fclose(fid);

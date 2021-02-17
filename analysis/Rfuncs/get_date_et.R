f8f0faa20e6c300cb093d7a508430ad6e9d149c3
Robin <sifre002@umn.edu>
2021-02-17 10:36:24 -0600

    expects .tsv as input

--- analysis/Rfuncs/get_date_et.R
+++ analysis/Rfuncs/get_date_et.R
@@ -1,26 +1,15 @@
-get_date_et <- function(data_dir){
-  # Function returns RecordingDate from raw tobii data 
-  # Input: Session folder 
-  session = basename(data_dir)
-  # Read raw .tobii
-  tobii_file = paste(data_dir, '/', session, '.txt', sep = '')
-  col_file = paste(data_dir, 'colnames.txt', sep = '/')
+get_date_et <- function(tobii_file){
+ # Input: tobii .tsv
   
   if (file.exists(tobii_file)) {
-    t = read.delim(tobii_file, nrows = 1, header = TRUE, sep = ',')
-    t = as.character(t$x)
-    t = strsplit(t, ',')
+    t = read.delim(tobii_file, nrows = 1, header = TRUE, sep = '\t')
+    recording_date = t$RecordingDate
     
-    # 
-    c = read.csv(col_file)
-    c = as.character(c$x)
-    c = strsplit(c, ',')
-    
-    datecol = which(grepl(x=c[[1]], pattern='RecordingDate'))
-    
-    # Pull date
-    return (t[[1]][datecol])
-  } else{
-    return('error')
+    if (!is.null(recording_date)) {
+      return(recording_date)
+    } else {
+      return('.tsv Missing RecordingDate col')
+    }
   }
+  
 }

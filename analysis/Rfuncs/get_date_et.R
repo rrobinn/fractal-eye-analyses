e816eae54b3a9c0dba33b3a522d002f64fd47489
Robin <sifre002@umn.edu>
2021-02-18 07:01:15 -0600

    tryCatch so it doesn't break when called in big loop

--- analysis/Rfuncs/get_date_et.R
+++ analysis/Rfuncs/get_date_et.R
@@ -1,15 +1,24 @@
 get_date_et <- function(tobii_file){
- # Input: tobii .tsv
+  # Input: tobii .tsv
   
   if (file.exists(tobii_file)) {
-    t = read.delim(tobii_file, nrows = 1, header = TRUE, sep = '\t')
-    recording_date = t$RecordingDate
+    t <- tryCatch(
+      read.delim(tobii_file, nrows = 1, header = TRUE, sep = '\t'),
+      error = function(e) 'Could no read file')
+    
+    if (is.data.frame(t)) {
+      recording_date = t$RecordingDate
+    } else {
+      rturn(t)
+    }
     
     if (!is.null(recording_date)) {
       return(recording_date)
     } else {
       return('.tsv Missing RecordingDate col')
     }
+  } else {
+    return('.tsv does not exist')
   }
   
 }
